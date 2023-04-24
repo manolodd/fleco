b9239736a2edd4f4cfc3027f551b9196924a26c6
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2023-04-24 13:48:34 +0200

    Changes in an import

--- src/main/java/com/manolodominguez/fleco/algorithm/FitnessEvaluatorFactory.java
+++ src/main/java/com/manolodominguez/fleco/algorithm/FitnessEvaluatorFactory.java
@@ -30,9 +30,9 @@
  */
 package com.manolodominguez.fleco.algorithm;
 
-import com.manolodominguez.fleco.genetic.Alleles;
-import com.manolodominguez.fleco.genetic.Chromosome;
-import com.manolodominguez.fleco.genetic.Genes;
+import com.manolodominguez.fleco.genetics.Alleles;
+import com.manolodominguez.fleco.genetics.Chromosome;
+import com.manolodominguez.fleco.genetics.Genes;
 import com.manolodominguez.fleco.strategicconstraints.StrategicConstraints;
 import com.manolodominguez.fleco.uleo.ImplementationGroups;
 import java.util.concurrent.CopyOnWriteArrayList;

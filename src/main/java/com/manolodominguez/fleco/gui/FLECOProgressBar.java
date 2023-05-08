bf821dba32f32eade668f5ffd336f05e53afd946
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2023-05-08 19:03:29 +0200

    Added to make easier the update of the progress bar of a FLECO algorithm in real time.

---
+++ src/main/java/com/manolodominguez/fleco/gui/FLECOProgressBar.java
@@ -0,0 +1,70 @@
+/* 
+ *******************************************************************************
+ * FLECO (Fast, Lightweight, and Efficient Cybersecurity Optimization) Adaptive, 
+ * Constrained, and Multi-objective Genetic Algorithm is a genetic algorithm  
+ * designed to assist the Asset's Cybersecurity Committee (ACC) in making 
+ * decisions during the application of CyberTOMP(1), aimed at managing 
+ * comprehensive cybersecurity at both tactical and operational levels.
+ *
+ * (1) Dominguez-Dorado, M., Carmona-Murillo, J., Cortés-Polo, D., and
+ * Rodríguez-Pérez, F. J. (2022). CyberTOMP: A novel systematic framework to
+ * manage asset-focused cybersecurity from tactical and operational levels. IEEE
+ * Access, 10, 122454-122485.
+ *******************************************************************************
+ * Copyright (C) Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this program. If not, see 
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html.
+ *******************************************************************************
+ */
+package com.manolodominguez.fleco.gui;
+
+import com.manolodominguez.fleco.events.ProgressEvent;
+import javax.swing.JProgressBar;
+import com.manolodominguez.fleco.events.IFLECOProgressEventListener;
+
+/**
+ * This class implements a progress bar that also is able to receive
+ * progressevents from an instance of FLECO algorithm.
+ *
+ * @author Manuel Domínguez-Dorado
+ */
+public class FLECOProgressBar extends JProgressBar implements IFLECOProgressEventListener {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * This is the constructor of the class. it creates a new instance of
+     * FLECOProgressBar and initialize its attributes.
+     *
+     * @author Manuel Domínguez-Dorado
+     */
+    public FLECOProgressBar() {
+        super();
+    }
+
+    /**
+     * This class is called when an instance of FLECO algorithm this progress
+     * bar is subscribed to, send a progress event.
+     *
+     * @param progressEvent the progress event sent by amn instance of FLECO
+     * algorithm.
+     * @author Manuel Domínguez-Dorado
+     */
+    @Override
+    public void onProgressEventReceived(ProgressEvent progressEvent) {
+        setValue((int) (progressEvent.getProgressPercentage() * 100));
+    }
+
+}

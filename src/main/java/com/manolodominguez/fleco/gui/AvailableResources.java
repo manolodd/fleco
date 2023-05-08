581d0188d4955a1933b46cf9ff828ab8af1a9244
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2023-05-08 18:51:20 +0200

    This class is added to handle easier the access to resources. And to change the path easilty if a refactor is needed.

---
+++ src/main/java/com/manolodominguez/fleco/gui/AvailableResources.java
@@ -0,0 +1,69 @@
+/* 
+ *******************************************************************************
+ * FLECO (Fast, Lightweight, and Efficient Cybersecurity Optimization) Adaptive, 
+ * Constrained, and Multi-objective Genetic Algorithm is a genetic algorithm  
+ * designed to assist the Asset's Cybersecurity Committee (ACC) in making 
+ * decisions during the application of CyberTOMP(1), aimed at managing 
+ * comprehensive cybersecurity at both tactical and operational levels.
+ *
+ * (1) Dominguez-Dorado, M., Carmona-Murillo, J., Cortés-Polo, D., and
+ * Rodríguez-Pérez, F. J. (2022). CyberTOMP: A novel systematic framework to
+ * manage asset-focused cybersecurity from tactical and operational levels. IEEE
+ * Access, 10, 122454-122485.
+ *******************************************************************************
+ * Copyright (C) Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this program. If not, see 
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html.
+ *******************************************************************************
+ */
+package com.manolodominguez.fleco.gui;
+
+/**
+ * This is an enum implementation to identify all available resources that can
+ * be used in FLECO.
+ *
+ * @author Manuel Domínguez-Dorado
+ */
+public enum AvailableResources {
+    FLECO_JSON_SCHEMA("FLECOJSONSchema.json");
+
+    private final String resourceFileName;
+
+    /**
+     * This is the constructor of the enum. It creates a new enum item and
+     * associates a filename to it.
+     *
+     * @param resourceFileName the filename that corresponds to the new
+     * available resource created.
+     * @author Manuel Domínguez-Dorado
+     */
+    private AvailableResources(String resourceFileName) {
+        this.resourceFileName = resourceFileName;
+    }
+
+    /**
+     * This methods gets the complete file path to the resource associated to
+     * the enum item.
+     *
+     * @author Manuel Domínguez-Dorado
+     * @return the complete file path to the resource associated to the enum
+     * item.
+     */
+    public String getResource() {
+        return AvailableResources.RESOURCES_PATH + resourceFileName;
+    }
+
+    private static final String RESOURCES_PATH = "/com/manolodominguez/fleco/json/";
+}
